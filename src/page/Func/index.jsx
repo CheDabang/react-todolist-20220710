5c58b7c953cd5df574002b62b747933ee05f5822
车大棒 <che_dabang@qq.com>
2022-08-17 00:02:24 +0800

    样式设置为左右布局

--- src/page/Func/index.jsx
+++ src/page/Func/index.jsx
@@ -40,7 +40,8 @@ export default function Func() {
   };
   return (
     <div>
-      <h1>函数式基础写法</h1>
+      <h1 className="todos-title">Todos</h1>
+      <h2>函数式基础写法</h2>
       <Head addTodo={addTodo} />
       <List todos={todos} updateTodo={updateTodo} deleteTodo={deleteTodo} />
       <Bottom
